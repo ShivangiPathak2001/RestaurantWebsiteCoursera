40f4961b5f647d0965ef409a4441cfd23ea0a108
Shivangi Pathak <shivangipathak929@gmail.com>
2021-05-16 12:48:50 +0530

    Added dynamically loading home page content code

--- Website/script.js
+++ Website/script.js
@@ -2,36 +2,48 @@ $(function(){
     $("#navbarToggle").blur(function (event){
         var screenwidth= window.innerWidth;
         if(screenwidth < 992){
-           $("#collapsable-nav").collapse('hide');
+           $("#collapsible-nav").collapse('hide');
         }
     });
-});
-(function(global){
-    var dc = {};
-    var homeHtml = "snippets/main-content.html";
-    
-    var insertHtml = function (selector, html){
-        var targetElem = document.querySelector(selector);
-        targetElem.insertHtml=html;
-    };
-
-    var showLoading = function (selector){
-        var html = "<div class ='text-center'>";
-        html+="<img src='ajax-loader.gif'></div>";
-        insertHtml(selector, html);
-    };
-
-    document.addEventListener("DOMContentLoaded", function(event){
-
-        showLoading("#main-content");
-        $ajaxUtils.sendGetRequest(
-            homeHtml,
-            function (responseText){
-                document.querySelector("#main-content").innerHTML = responseText;
-
-            },
-            false)
+    $("#navbarToggle").click(function (event) {
+        $(event.target).focus();
+      });
     });
-    global.$dc=dc;
-
-})(window);
+    
+    (function (global) {
+    
+    var dc = {};
+    
+    var homeHtml = "/js/snippets/Home-page.html";
+    
+    // Convenience function for inserting innerHTML for 'select'
+    var insertHtml = function (selector, html) {
+      var targetElem = document.querySelector(selector);
+      targetElem.innerHTML = html;
+    };
+    
+    // Show loading icon inside element identified by 'selector'.
+    var showLoading = function (selector) {
+      var html = "<div class='text-center'>";
+      html += "<img src='ajax-loader.gif'></div>";
+      insertHtml(selector, html);
+    };
+    
+    // On page load (before images or CSS)
+    document.addEventListener("DOMContentLoaded", function (event) {
+    
+    // On first load, show home view
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+      homeHtml,
+      function (responseText) {
+        document.querySelector("#main-content")
+          .innerHTML = responseText;
+      },
+      false);
+    });
+    
+    
+    global.$dc = dc;
+    
+    })(window);
